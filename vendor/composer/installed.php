a5712741180d47310e268d7f26dcb65b20799059
joemgreen22 <joemgreen22@gmail.com>
2020-11-03 14:33:09 -0700

    sendgrid6

---
+++ vendor/composer/installed.php
@@ -0,0 +1,49 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a71a9192064511bda9e73e278785ece2831da50c',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a71a9192064511bda9e73e278785ece2831da50c',
+    ),
+    'sendgrid/php-http-client' => 
+    array (
+      'pretty_version' => '3.1.0',
+      'version' => '3.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '01c3fc4b94df008cfe03ffb467d4c6dbe6dcfb2c',
+    ),
+    'sendgrid/sendgrid' => 
+    array (
+      'pretty_version' => '5.0.9',
+      'version' => '5.0.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fbf43e7851463c49d6d45382151fbd2e90028af3',
+    ),
+    'sendgrid/sendgrid-php' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+  ),
+);
