053821eb0043af62cdf572a191c57dbc8de06877
joemgreen22 <joemgreen22@gmail.com>
2020-11-04 01:34:39 -0700

    sendgrid10

--- createUser_handler.php
+++ createUser_handler.php
@@ -5,10 +5,12 @@ $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 require_once 'Dao.php';
 
+echo 'create handler' . '\n';
 //valid email
 $pattern = '^[a-z|A-Z|0-9]+@[a-z|A-Z|0-9|.]+\.[com|org|net|edu]{3}^';
 preg_match($pattern, $_POST['email'], $matches) ;
 if($matches==null){
+    echo 'invalid email' . '\n';
     $_SESSION['badUser'][] = "This is not a valid email";
     $_SESSION['authenticated'] = false;
 
@@ -20,6 +22,7 @@ if($matches==null){
 
 //valid name
 if(strlen($_POST['nameFirst']) <=0 || strlen($_POST['nameLast']) <=0){
+    echo 'invalid name' . '\n';
     $_SESSION['badUser'][] = "This is not a valid name";
     $_SESSION['authenticated'] = false;
     $extra = 'login.php';
@@ -32,6 +35,7 @@ if(strlen($_POST['nameFirst']) <=0 || strlen($_POST['nameLast']) <=0){
 $dao = new Dao();
 $results = $dao->userExists($_POST['email'], $_POST['password']);
 if(count($results)> 0){
+    echo 'invalid emial2' . '\n';
     $_SESSION['badUser'][] = "This email is already in use";
     $_SESSION['authenticated'] = false;
     $extra = 'login.php';
@@ -40,6 +44,7 @@ if(count($results)> 0){
     exit();
 }
 if(count($results)<= 0){
+    echo 'valid create' . '\n';
     $dao->createUser($_POST['nameFirst'], $_POST['nameLast'], $_POST['email'], $_POST['password']);
     $_SESSION['authenticated'] = true;
     $extra = 'home.php';
